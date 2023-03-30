40292d6f1996fb47d66c29281a64012a415055c2
MeeskaM00ska <nathannnnn.li@mail.utoronto.ca>
2023-03-30 08:53:24 -0400

    Update help.js

--- routes/help.js
+++ routes/help.js
@@ -4,21 +4,25 @@ const { pool } = require('../dbConfig');
 const ver_tools = require('../tools/verifiers');
 
 router.get('/', (req, res) => {
-    const pid = ver_tools.login_ver(req.headers.authorization.split(' ')[1]);
-    if (pid < 0) {
+    try {
+        const pid = ver_tools.login_ver(req.headers.authorization.split(' ')[1]);
+        if (pid < 0) {
+            return res.status(403).send({ message: 'Invalid credentials' });
+        }
+        pool.query('SELECT * FROM Help')
+          .then((helpdata) => {
+              const dataList = helpdata.rows.map((row) => ({
+                  question: row.question,
+                  answer: row.answer
+              }));
+              return res.status(200).json({ message: 'Retrive All Help Info Successfully', help: dataList });
+          }).catch((error) => {
+            console.log(error);
+            return res.status(500).send({ message: 'Occur errors when getting relevant data' });
+        })
+    } catch (err) {
         return res.status(403).send({ message: 'Invalid credentials' });
     }
-    pool.query('SELECT * FROM Help')
-    .then((helpdata) => {
-        const dataList = helpdata.rows.map((row) => ({
-            question: row.question,
-            answer: row.answer
-        }));
-        return res.status(200).json({ message: 'Retrive All Help Info Successfully', help: dataList });
-    }).catch((error) => {
-        console.log(error);
-        return res.status(500).send({ message: 'Occur errors when getting relevant data' });
-    })
 })
 
 module.exports = router;
